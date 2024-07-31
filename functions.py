2270c5a7db0589913bbce9e45408c813e9e33181
Nam Le <nambaole3@gmail.com>
2024-07-30 20:41:07 -0700

    adding files

---
+++ functions.py
@@ -0,0 +1,121 @@
+from math import *
+import numpy as np
+import random
+from termcolor import *
+
+Any = 'SIAN'
+Array = 'SAN'
+List = 'AN'
+Const = 'SA'
+
+lambdas = {
+	'+': [
+		[['N', 'N'], "y + x"],
+		[['S', 'S'], "y + x"],
+		[['A', 'A'], "y + x"],
+		[[Array, 'I'], "y[-x % len(y):] + y[:-x % len(y)]"], 
+	 ], '-': [
+		[['I', 'I'], "y - x"],
+		[[Array, Any], "find(y, x)"],
+	 ], '*': [
+		[['I', 'I'], "y * x"],
+		[[Array], "random.sample(x, len(x))"],
+	 ], '/': [
+		[['I', 'I'], "y / x"],
+		[[Array, 'I'], "[y[i:i + x] for i in range(len(y) - x + 1)]"],
+		[[List], "np.ravel(x).tolist()"],
+		[['S'], "x.upper()"]
+	 ], '^': [
+		[['I', 'I'], "y ** x"],
+		[[Array], "[i for n, i in enumerate(x) if i not in x[:n]]"],
+	 ], '\'': [
+		[['I'], "ceil(x)"],
+		[[Array], "x[-1]"],
+	 ], '_': [
+		[['I'], "floor(x)"],
+		[[Array], "x[0]"],
+	 ], '{': [
+		[[Const, Const], "min([y, x])"],
+		[[Array], "sorted(x)"],
+	 ], '}': [
+		[[Const, Const], "min([y, x])"],
+		[['N'], "sorted(x, reverse = True)"],
+	 ], '%': [
+		[['I', 'I'], "y % x"],
+		[[Array, Const], "x in y"],
+	 ], '!': [
+		[[Array], "reversed(x)"]
+	 ], '?': [
+		[['I'], "random.randint(1, x) if x != 0 else random.random()"],
+		[[Array], "where(x)"],
+	 ], '@': [
+		[[Array, 'I'], "y[x]"],
+		[[Array, 'N'], "np.reshape(y, tuple(x)).tolist()"],
+	 ], '#': [
+		 [['I'], "range(1, x + 1)"],
+		 [['A'], "len(x)"]
+	 ], '$': [
+		 [['S'], "int(x)"],
+		 [['I'], "str(x)"],
+	 ], '[': [
+		 [[], "{}"]
+   ], ']': [
+		 [[], "idx = stack.rindex({})\ndel stack[idx]\n[i[1] for i in stack[idx + 1:]]", lambda x: len(x) - rindex(x, ('C', {}))]
+   ]
+}
+
+def runFunc(token, stack):
+	classes = ['SI'[type(i[1]) == int] if i[0] == 'C' else ('AN'[all([type(j) == int for j in i[1]])] if i[0] == 'A' else i[0]) for i in stack]
+	types = [type(i[1]) for i in stack]
+	values = [i[1] for i in stack]
+	#debug(f"{values} {types} {classes}")
+	
+	# Find function
+	functions = lambdas[token]
+	for data in functions:
+		funcTypes = list(zip(classes[-max([len(classes) - len(data[0]), 0]):], data[0]))
+		funcTypes = [(i[0] in i[1]) for i in funcTypes]
+		matched = []
+		if (not data[0]) or all(funcTypes):
+			matched = data; pops = len(data[0]) if len(data) == 2 else data[2](stack); break
+		
+	# Perform function
+	assert matched, colored('Wrong types for function ' + token, 'red', attrs=['bold'])
+	args = len(matched[0])
+	if args >= 1: x = values[-1]
+	if args >= 2: y = values[-2]
+	if args >= 3: z = values[-3]
+	if args >= 4: t = values[-4]
+	idx = 0
+	function = matched[1]
+	split = function.split('\n')
+	try: exec(split[:-2])
+	except: pass
+	value = eval(split[-1])
+	del stack[-pops:]
+	stack += [typed(value)]
+	debug(stack)
+	return stack
+
+def find(array, elem):
+	result = []
+	lenElem = len(elem) if type(elem) != int else 1
+	for i in range(len(array) - lenElem + 1):
+		if type(elem) == int: elem = [elem]
+		result.append(int(array[i:i + lenElem] == elem))
+	result += [0] * (lenElem - 1)
+	return result
+
+typed = lambda x: ('CA'[type(x) == list], x)
+islist = lambda x: type(x) in [list, str]
+where = lambda x: [i for i, j in enumerate(x) if j] if np.ndim(x) == 1 else [encodeRadix(list(np.shape(x)), i) for i, j in enumerate(np.ravel(x)) if j]
+
+def encodeRadix(alpha, omega):
+	alpha.reverse()
+	result = []
+	for a in alpha:
+		omega, rem = divmod(omega, a); result += [rem]
+	return list(reversed(result))
+
+debug = lambda x: print(colored("Debug: ", 'red') + colored(str(x), 'cyan'))
+rindex = lambda a, b: len(a) - a[-1::-1].index(b) - 1
---
+++ main.py
@@ -0,0 +1,111 @@
+from functions import *
+
+def run(lines):
+	global stack, predef
+	predef = {
+		'symbols': '"',
+		'functions': '[]+-*/^\'_{}|%!?@#$~&\\=<>,:;() ',
+		'parsed': '',
+		'array': [],
+		'tokens': [],
+		'token': ''
+	}
+
+	# Tokenize
+	for line in lines:
+		for char in line:
+			if char in (predef['symbols'] + predef['functions']): # Check if symbol
+				if predef['token']: predef['tokens'] += [('CONST', predef['token'])]
+				predef['tokens'] += [('SMB' if char in predef['symbols'] else 'FUNC', char)]
+				predef['token'] = ''
+			else:
+				predef['token'] += char
+		if predef['token']: predef['tokens'] += [('CONST', predef['token'])]
+		predef['token'] = ''
+
+	# Group
+	groups = group(predef['tokens'])
+	parser = parse(groups)
+
+	# Typed parsed tokens
+	types = typeCode(parser)
+	types = [deepBool(i) for i in types]
+	types = ['CFAB'[2*(type(i) == list) + (not i if type(i) != list else not i[0])] for i in types]
+	types = list(zip(types, parser))
+	types = [(i[0], trueType(i[1])) if i[0] in 'CA' else i for i in types]
+
+	# Run
+	runCode(types)
+	
+def trueType(array):
+	if type(array) == str:
+		return array[1:-1] if array[0] == '"' else array
+	elif type(array) not in [str, list]:
+		return array
+	return [trueType(i) for i in array]
+
+def typeCode(parsedCode):
+	global predef
+	if type(parsedCode) != list:
+		if type(parsedCode) in [float, int]: return True
+		elif type(parsedCode) == str: return parsedCode not in predef['functions']
+	return [typeCode(token) for token in parsedCode]
+
+deepBool = lambda x: [False not in x] if type(x) == list else x
+
+def runCode(parsedCode):
+	stack = []
+	for token in parsedCode:
+		tokenType = token[0]
+		token = token[1]
+		if tokenType in 'CAB': 
+			stack += [(tokenType, token)]
+			try: runFunc(token, stack)
+			except: pass
+		else: stack = runFunc(token, stack)
+	print(f'{'{\n' + ''.join(['  ' + str(i[1]) +',\n' for i in stack]) + '}'}')
+
+def group(tokens):
+	opened = ''
+	resGroup = []
+	if len(tokens) < 1 or not max([i[0] == 'SMB' for i in tokens]) or tokens[0] == '$STR':
+		return tokens
+	for token in tokens:
+		tokenType = token[0]
+		tokenChar = token[1]
+		if tokenType == 'SMB': # Check for open-close symbols
+			if len(opened) == 0: resGroup += [[]]
+			if tokenChar == '"':
+				if opened and opened[-1] == '"': 
+					opened = opened[:-1]
+					if len(opened) >= 1: resGroup[-1] += [token]
+				else: 
+					if len(opened) >= 1: resGroup[-1] += [token]
+					else: resGroup[-1] += ['$STR']
+					opened += tokenChar
+		elif tokenType in ['CONST', 'FUNC', '$']: # Keep lists, constants, and functions
+			if not opened: resGroup += [token]
+			else: resGroup[-1] += [token]
+	resGroup = [group(i) for i in resGroup]
+	return resGroup
+
+def parse(groups):
+	resGroup = []
+	for idx, group in enumerate(groups):
+		if group[0] == 'CONST' or (group[0] == 'FUNC' and group[1] != ' '):
+			try: resGroup += [eval(group[1].replace('`', '-'))]
+			except: resGroup += [group[1]]
+		elif group[0] == '$LIST':
+			resGroup += [parse(groups[idx][1:])]
+		elif group[0] == '$STR':
+			resGroup += [f"\"{''.join([i[1] for i in groups[idx][1:]])}\""]
+	return resGroup
+
+def main():
+	global stack, file
+	stack = []
+	file = open("run.sbx")
+	run(file.read().split("\n"))
+
+if __name__ == '__main__':
+  main()
